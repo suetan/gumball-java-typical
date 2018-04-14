1564939be8caf14d7c4596ed6192c62d46d44568
Su Tan <sutan@Sus-MacBook-Pro.local>
2018-04-14 09:23:09 -0700

    updated Gumball Machine

--- src/GumballMachine.java
+++ src/GumballMachine.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /*
  * GumballMachine simulation
@@ -5,13 +6,19 @@
  * Date : April 3, 2018
  * Version 1 : https://github.com/paulnguyen/cmpe202/tree/master/gumball/gumball-java-typical
  * Description : Functionalities of GumballMachine
- *               - insertQuarter
- *               - turnCrank
+ *               - insertQuarter function for 25 cents gumball
+ *               - insertCoins function for 50 cents and smaller change (5 or 10 or 25)
+ *               - turnCrank function for ejecting gumball
  */
 public class GumballMachine {
 	private int num_gumballs;
     private boolean has_quarter;
-
+    
+  //  private int money = 0; //for nickel, dime, and quarter
+   // private int total =0;
+    
+    Scanner keyboard = new Scanner(System.in);
+    
     public GumballMachine( int size )
     {
         // initialize instance variables
@@ -21,23 +28,69 @@ public class GumballMachine {
 
     public void insertQuarter(int coin)
     {
-        if ( coin == 25 )
+    	System.out.println(coin);
+        if ( coin == 50) 
             this.has_quarter = true ;
+        
+        else if  (coin == 25)
+        	this.has_quarter = true;
         else 
-            this.has_quarter = false ;
+        {	
+     	     this.has_quarter = false ;
+        }    
+     	      
     }
     
+    public void insertCoins()
+    {
+    	int total = 0; 
+    	int money = 0;
+    	boolean flag = true;
+		
+		while ( total < 50)
+		{
+			System.out.println("Insert coins (5 or 10 or 25) cents"); 
+			money = keyboard.nextInt();
+			if((money == 5) || (money == 10) || (money == 25))
+			{
+				total += money;
+				
+			}
+			else
+				System.out.println("Coins should be 5 or 10 or 25 cents|");
+		}	
+			
+			
+		System.out.println("Here is the total : " + total + " cent(s)");
+	
+		
+		
+		// greater than or equal 50 cents
+		if (total >= 50)
+		{
+				total = total - 50;
+				System.out.println("Here is your change " + total + " cent(s)");
+				this.has_quarter =true;
+		}
+			
+		else
+			this.has_quarter = false;
+				
+	}	
+			
+	
+    
+ 
     public void turnCrank()
     {
-    	if ( this.has_quarter )
+    	if ( this.has_quarter)
     	{
     		if ( this.num_gumballs > 0 )
     		{
     			this.num_gumballs-- ;
     			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    			//
-    			//System.out.println("Gumball_count = "+ this.num_gumballs);
+    			System.out.println( "Thanks. Gumball Ejected!" ) ;
+    			
     		}
     		else
     		{
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,48 @@
+/*
+ * GumballMachine simulation
+ * Writer : Su Tan
+ * Date : April 3, 2018
+ * Version 1 : https://github.com/paulnguyen/cmpe202/tree/master/gumball/gumball-java-typical
+ * Description : Functionalities of GumballMachine
+ *               - first 25 cents gumball
+ *               - second 50 cents gumball (that also accepts smaller changes (5 or 10 or 25 cents)
+ *               - third 25 cents gumball (check for no gumball state)
+ */
 
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {// TODO Auto-generated method stub
-		GumballMachine gumballMachine = new GumballMachine(5);
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		
+		GumballMachine gumballMachine = new GumballMachine(2);
 
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter( 25 );
+		System.out.println("---------------First 25 cents Gumball---------------------");
+		
+		
+		// first gumballMachine for 25 cents
+			gumballMachine.insertQuarter( 25 );
+			gumballMachine.turnCrank();
+			//System.out.println(gumballMachine);
+			
+				
+		//second gumball for 50 cents (5 or 10 or 25 cents)
+		
+		System.out.println("-----------------Second 50 cents Gumball-----------------------");
+		gumballMachine.insertCoins();
 		gumballMachine.turnCrank();
+			
+		// third gumball (gumballmachine only have 2 gumballs)
+		System.out.println("------Third 25 cents Gumball (no gumball state)-------");
+			gumballMachine.insertQuarter( 25 );
+			gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
+		keyboard.close();
+		
+		
+		
 	}
 
 }
